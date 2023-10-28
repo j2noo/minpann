4dd267decb79897cd63b335d4a6cd16244363139
j2noo <wlsdndml213@naver.com>
2023-10-29 00:19:44 +0900

    feat : 라우팅 설정

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Start from "./pages/Start";
+
+function Router() {
+  // const a = 1;
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Start/>} />
+        <Route path="/login" element={<div>login</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
