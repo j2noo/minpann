489c0f702c6a74b1695d0c42c463aff8485a9256
j2noo <wlsdndml213@naver.com>
2023-11-14 18:07:43 +0900

    Feat : 배경 이미지 배열 예시 추가

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -2,10 +2,13 @@ import { useRecoilValue } from "recoil";
 import { currPageState } from "../atom";
 import Question from "./Question";
 import img1 from "/assets/bg.png";
+import img2 from "/assets/bg2.png";
+import img3 from "/assets/bg3.png";
+import img4 from "/assets/bg4.png";
 
 const Start = () => {
   const answers = ["답1", "답2", "답3", "안녕"];
-  const imgSrcs = [img1, img1, img1, img1];
+  const imgSrcs = [img1, img2, img3, img4];
   const currPage = useRecoilValue(currPageState);
 
   let content;
