a316d77b901df15dc011ce0fb6810a684cc67a4a
j2noo <wlsdndml213@naver.com>
2023-12-06 23:46:26 +0900

    feat : 이미지 파일 추가

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,14 +1,53 @@
 import { useRecoilValue } from "recoil";
 import { currPageState } from "../atom";
 import Question from "./Question";
-import img1 from "/assets/bg.png";
-import img2 from "/assets/bg2.png";
-import img3 from "/assets/bg3.png";
-import img4 from "/assets/bg4.png";
+import img1 from "/assets/1.png";
+import img2 from "/assets/2.png";
+import img3 from "/assets/3.png";
+import img4 from "/assets/4.png";
+import img5 from "/assets/5.png";
+import img6 from "/assets/6.png";
+import img7 from "/assets/7.png";
+import img8 from "/assets/8.png";
+import img9 from "/assets/9.png";
+import img10 from "/assets/10.png";
+import img11 from "/assets/11.png";
+import img12 from "/assets/12.png";
+import img13 from "/assets/13.png";
+import last from "/assets/last.png";
 
 const Start = () => {
-  const answers = ["답1", "답2", "답3", "안녕ws"];
-  const imgSrcs = [img1, img2, img3, img4];
+  const answers = [
+    "*",
+    "라치오스",
+    "*",
+    "lion",
+    "3",
+    "국제관",
+    "108",
+    "박물관옆",
+    "1004",
+    "*",
+    "B116",
+    "0810",
+    "*",
+  ];
+  const imgSrcs = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    last,
+  ];
   const currPage = useRecoilValue(currPageState);
 
   let content;
