0fd9c788b404fcdfc636327e237ad243d3538c0d
j2noo <wlsdndml213@naver.com>
2023-12-13 23:41:50 +0900

    fix : 확장자 변경

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -3,12 +3,12 @@ import { currPageState } from "../atom";
 import Question from "./Question";
 import styled from "styled-components";
 
-import img1 from "/assets/1.png";
+import img1 from "/assets/1.jpg";
 import img2 from "/assets/2.png";
-import img3 from "/assets/3.png";
+import img3 from "/assets/3.jpg";
 import img4 from "/assets/4.png";
-import img5 from "/assets/5.png";
-import img6 from "/assets/6.png";
+import img5 from "/assets/5.jpg";
+import img6 from "/assets/6.jpg";
 import img7 from "/assets/7.png";
 import img8 from "/assets/8.png";
 import img9 from "/assets/9.png";
