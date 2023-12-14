e45bfd74216fc7d118133d1920d36be547cea374
j2noo <wlsdndml213@naver.com>
2023-12-14 20:36:39 +0900

    feat  : 사진 수정

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -7,7 +7,7 @@ import img1 from "/assets/1.jpg";
 import img2 from "/assets/2.png";
 import img3 from "/assets/3.jpg";
 import img4 from "/assets/4.png";
-import img5 from "/assets/5.jpg";
+import img5 from "/assets/5.png";
 import img6 from "/assets/6.jpg";
 import img7 from "/assets/7.png";
 import img8 from "/assets/8.png";
